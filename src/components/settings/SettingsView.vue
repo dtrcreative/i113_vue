4efee96fdb44e795ff6fa949d939af2b5d502f18
drogozhnikov <DitarRogozhnikov@gmail.com>
2023-12-26 14:58:43 +0300

    Added: tabs language and sheduling

--- src/components/settings/SettingsView.vue
+++ src/components/settings/SettingsView.vue
@@ -1,24 +1,44 @@
 <template>
-  <div class="sections">
-    <div class="section-left">
-      <div class="block">
+  <v-card>
+    <v-tabs v-model="tab" bg-color="primary" fixed-tabs class="tabbar">
+      <v-tab value="language">Language</v-tab>
+      <v-tab value="Sheduling">Sheduling</v-tab>
+    </v-tabs>
 
-      </div>
-    </div>
-    <div class="section-right">
-      <div class="block">
+    <v-card-text>
+      <v-window v-model="tab">
 
-      </div>
-    </div>
-  </div>
+        <v-window-item value="language">
+          Language
+        </v-window-item>
+
+        <v-window-item value="Sheduling">
+          Sheduling
+        </v-window-item>
+
+      </v-window>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
+
+import PandaMainView from "@/components/microservices/panda/PandaMainView";
 export default {
-  name: "SettingsView"
+  name: "SettingsView",
+  components: {PandaMainView},
+  data: () => ({
+    tab: null,
+  }),
+  methods: {}
 }
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
+@import '../../assets/styles/main'
+
+.tabbar
+  border-bottom-left-radius: 20px
+  border-bottom-right-radius: 20px
 
 </style>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,13 +1,48 @@
 <template>
-
+  <settings-view></settings-view>
 </template>
 
 <script>
+import SettingsView from "@/components/settings/SettingsView";
+
 export default {
-  name: "Settings"
+  name: "Settings",
+  components: {SettingsView}
 }
 </script>
 
-<style scoped>
+
+<style lang="sass" scoped>
+@import '../assets/styles/main'
+
+.page
+  position: relative
+  display: flex
+  justify-content: center
+  align-items: center
+  min-height: 100vh
+  padding: 20px 0
+  background-color: #59585d
+
+.sections
+  display: flex
+  width: 100%
+
+  @media (max-width: 767px)
+    flex-direction: column
+
+.section-center
+  width: 100%
+  margin-top: 10px
+
+  @media (max-width: 767px)
+    width: 100%
+
+.overflow-visible
+  height: 50px
+  margin-right: 10px
+  margin-left: 10px
+  border-bottom-left-radius: 25px
+  border-bottom-right-radius: 25px
 
 </style>
