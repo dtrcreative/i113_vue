3cb250c28260a77281556cf8c489fc11440428ac
drogozhnikov <DitarRogozhnikov@gmail.com>
2024-04-03 16:17:28 +0300

    Updated: parametrNames

--- src/components/auth/services/axios-helper.js
+++ src/components/auth/services/axios-helper.js
@@ -1,27 +1,27 @@
 import userHelper from "@/components/auth/services/user.helper";
 
-// const ActiveProfile = "dev"
-const ActiveProfile = "prod"
+const ActiveProfile = "dev"
+// const ActiveProfile = "prod"
 
-const GATEWAY_URL_TEST = 'http://localhost:8080/';
+const GATEWAY_URL_DEV = 'http://localhost:8080/';
 const GATEWAY_URL_PROD = 'http://192.168.100.9:8080/';
 
-const KEYCLOAK_URL_TEST = "http://localhost:3000/";
+const KEYCLOAK_URL_DEV = "http://localhost:3000/";
 const KEYCLOAK_URL_PROD = "http://192.168.100.9:3000/";
 
-const KEYCLOAK_URL_REALM_TEST = "http://localhost:28080/realms/i113_realm";
+const KEYCLOAK_URL_REALM_DEV = "http://localhost:28080/realms/i113_realm";
 const KEYCLOAK_URL_REALM_PROD = "http://192.168.100.9:28080/realms/i113_realm_prod";
 
 export const getGatewayUrl = () => {
-  return ActiveProfile === "prod" ? GATEWAY_URL_PROD : GATEWAY_URL_TEST;
+  return ActiveProfile === "prod" ? GATEWAY_URL_PROD : GATEWAY_URL_DEV;
 }
 
 export const getKeycloakUrl = () => {
-  return ActiveProfile === "prod" ? KEYCLOAK_URL_PROD : KEYCLOAK_URL_TEST;
+  return ActiveProfile === "prod" ? KEYCLOAK_URL_PROD : KEYCLOAK_URL_DEV;
 }
 
 export const getKeycloakRealmUrl = () => {
-  return ActiveProfile === "prod" ? KEYCLOAK_URL_REALM_PROD : KEYCLOAK_URL_REALM_TEST;
+  return ActiveProfile === "prod" ? KEYCLOAK_URL_REALM_PROD : KEYCLOAK_URL_REALM_DEV;
 }
 
 export const authHeader = () => {
