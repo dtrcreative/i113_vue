1b3e6c3e669cd8f4ec9bcd05ed1cba2fba0ef537
drogozhnikov <DitarRogozhnikov@gmail.com>
2024-04-04 15:06:25 +0300

    Refact: axios helper to property prod

--- src/components/auth/services/axios-helper.js
+++ src/components/auth/services/axios-helper.js
@@ -1,7 +1,7 @@
 import userHelper from "@/components/auth/services/user.helper";
 
-const ActiveProfile = "dev"
-// const ActiveProfile = "prod"
+// const ActiveProfile = "dev"
+const ActiveProfile = "prod"
 
 const GATEWAY_URL_DEV = 'http://localhost:8080/';
 const GATEWAY_URL_PROD = 'http://192.168.100.9:8080/';
