a542d3a4c3b5e64c2e998567431dadbc86079f9b
drogozhnikov <DitarRogozhnikov@gmail.com>
2024-04-28 14:16:20 +0300

    deploy

--- src/store/app.service.js
+++ src/store/app.service.js
@@ -4,7 +4,9 @@ const ActiveProfile = "prod"
 const GATEWAY_URL_DEV =         'http://localhost:8080/';
 const GATEWAY_URL_PROD =        'http://192.168.192.81:18080/';
 
-
+export const getGatewayUrl = () => {
+  return ActiveProfile === "prod" ? GATEWAY_URL_PROD : GATEWAY_URL_DEV;
+}
 
 export const getCurentDateString = () => {
   let currentDate = new Date().toString().split(" ")
