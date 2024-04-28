dc4d84354c6ab3cda2e570b2cb73941d8910a903
drogozhnikov <DitarRogozhnikov@gmail.com>
2024-04-28 15:20:08 +0300

    Fix: secutity port

--- src/store/app.service.js
+++ src/store/app.service.js
@@ -2,7 +2,7 @@
 const ActiveProfile = "prod"
 
 const GATEWAY_URL_DEV =         'http://localhost:8080/';
-const GATEWAY_URL_PROD =        'http://192.168.192.81:18080/';
+const GATEWAY_URL_PROD =        'http://192.168.192.81:18090/';
 
 export const getGatewayUrl = () => {
   return ActiveProfile === "prod" ? GATEWAY_URL_PROD : GATEWAY_URL_DEV;
