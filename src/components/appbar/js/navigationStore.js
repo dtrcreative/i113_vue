7e213bd9f7235784b93fe4ef56982cf456d344ab
drogozhnikov <DitarRogozhnikov@gmail.com>
2024-05-01 14:13:02 +0300

    Update vuetify

--- src/components/appbar/js/navigationStore.js
+++ src/components/appbar/js/navigationStore.js
@@ -11,6 +11,7 @@ export const useNavStore = defineStore('navigation', {
     microPages: [
       {title: 'Panda', value: "panda", icon: 'mdi-panda', color: 'orange', redirect: '/panda/'},
       {title: 'Events', value: "events", icon: 'mdi-calendar', color: 'red', redirect: '/events/'},
+      {title: 'Events_new', value: "events", icon: 'mdi-calendar', color: 'purple', redirect: '/events_new/'},
     ]
 
   }),
