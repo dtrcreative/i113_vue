3f04ac045429fc7f831ab4537ba1ce67fc1d8d32
drogozhnikov <DitarRogozhnikov@gmail.com>
2024-05-03 15:24:40 +0300

    Added: activeProfile view component

--- src/store/app.service.js
+++ src/store/app.service.js
@@ -1,5 +1,5 @@
-// const ActiveProfile = "dev"
-const ActiveProfile = "prod"
+const ActiveProfile = "dev"
+// const ActiveProfile = "prod"
 
 const GATEWAY_URL_DEV =         'http://localhost:8080/';
 const GATEWAY_URL_PROD =        'http://192.168.192.81:18080/';
@@ -14,4 +14,8 @@ export const getCurentDateString = () => {
         currentDate[1] + " " +
         currentDate[2] + " " +
         currentDate[3]
+}
+
+export const getActiveProfile = () => {
+  return ActiveProfile.toString()
 }
