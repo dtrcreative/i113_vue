b533a9d906cee4f5ebba31b5aef15ed9ab2be48a
drogozhnikov <DitarRogozhnikov@gmail.com>
2024-05-22 15:41:30 +0300

    Added: Vue EightBall

---
+++ src/components/home/EightBall.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-card>
+    <v-container>
+      <v-row>
+        <v-text-field
+          hide-details
+          type="number"
+          label="Question"
+          variant="outlined"
+          density="compact"
+          v-model="input"
+        ></v-text-field>
+      </v-row>
+      <v-row>
+        <v-btn
+          text="Get answer"
+          size="large"
+          variant="text"
+          rounded="xl"
+          @click="ask"
+        >
+        </v-btn>
+      </v-row>
+      <v-row>
+        <v-text-field
+          hide-details
+          type="text"
+          label="Answer"
+          variant="outlined"
+          density="compact"
+          v-model="output"
+        ></v-text-field>
+      </v-row>
+    </v-container>
+  </v-card>
+
+</template>
+
+<script>
+export default {
+  name: "EightBall",
+  data() {
+    return {
+      input: '',
+      output: '',
+    }
+  },
+  methods:{
+    ask(){
+
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import '@/assets/styles/main'
+
+.v-btn
+  margin: auto auto auto auto
+
+.v-card
+  background-color: $background-transparent
+
+.v-text-field
+  width: 20%
+  padding: 5px 0 5px 0
+</style>
